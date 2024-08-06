17d5a04f2dbacedad96926e30c0de89437810f84
Claudio faria <claudio.dantas18@gmail.com>
2024-08-06 11:02:31 -0300

    completo

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,29 @@
-/* main.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern int yylex();
+extern void yy_scan_string(const char *str);
+extern void yylex_destroy();
+
+void process_line(const char *line) {
+
+    yy_scan_string(line);
+
+    while (yylex() != 0) {
+
+    }
+    yylex_destroy();
+}
+
+int main() {
+    char line[1024];
+
+    while (fgets(line, sizeof(line), stdin)) {
+        process_line(line);
+    }
+
+    return 0;
+}
 
